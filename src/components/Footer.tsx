82e9b2a5a31dc0258b093d0b3d3772913737763d
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-08-29 23:15:14 +0200

    Implement Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,42 @@
+import Link from "next/link";
+
 export function Footer() {
   return (
     <>
-      <div className="bg-black text-white">
-        <div className="flex items-center justify-between container mx-auto px-24 py-4">
-          <div className="uppercase">Agentur und Leistungen</div>
-          <div className="uppercase">Arbeiten bei Apevertising</div>
-          <div className="uppercase">Kontakt</div>
+      <div className="bg-black text-white py-8">
+        <div className="grid grid-cols-3 gap-4 container mx-auto px-24 py-4">
+          <div className="grid grid-rows-5 gap-4">
+            <div className="uppercase">Agentur und Leistungen</div>
+            <Link href="/">Agentur</Link>
+            <Link href="/de/leistungen">Leistungen</Link>
+            <Link href="/">Referenzen</Link>
+            <div>
+              <Link href="/" className="uppercase border border-white p-2">
+                <img src="/WhiteLine.png" className="inline" />
+                <span>&nbsp;Newsletter abonnieren</span>
+              </Link>
+            </div>
+          </div>
+          <div className="grid grid-rows-5 gap-4">
+            <div className="uppercase">Arbeiten bei Apevertising</div>
+            <Link href="/de/heartworking">Heartworking</Link>
+            <Link href="/de/jobs">Jobs</Link>
+            <Link href="/de/beanape">#BEANAPE</Link>
+          </div>
+          <div className="grid grid-rows-5 gap-4">
+            <div className="uppercase">Kontakt</div>
+            <div>APEvertising</div>
+            <div>Lichtenhaidstr. 11a | 96052 Bamberg</div>
+            <div>E: birgitandiel@goldwert-ba.de</div>
+            <div>P: + 49 951 180 830 48 | M: + 49 162 42 46 091</div>
+          </div>
         </div>
       </div>
       <div className="bg-white text-black">
         <div className="flex items-center justify-between container mx-auto px-24 py-4">
-          <div className="corporate-font1">Impressum | Datenschutzerklärung | AGB | Hinweis nach VSBG</div>
+          <div className="corporate-font1">
+            Impressum&nbsp;&nbsp;|&nbsp;&nbsp;Datenschutzerklärung&nbsp;&nbsp;|&nbsp;&nbsp;AGB&nbsp;&nbsp;|&nbsp;&nbsp;Hinweis nach VSBG
+          </div>
           <div className="flex">
             <div className="corporate-font1">Follow us</div>
             <img src="/SocialMedia.png" alt="Social Media" width="220" />
