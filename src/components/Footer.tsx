f0de3fec17bbd9e5c07c0336f57e07c2ec9c6ab5
alfarrobeira <122460653+alfarrobeira@users.noreply.github.com>
2023-09-10 15:30:11 +0200

    Make Footer responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,26 @@ export function Footer() {
   return (
     <>
       <div className="bg-black text-white py-8">
-        <div className="grid grid-cols-3 gap-4 container mx-auto px-24 py-4">
-          <div className="grid grid-rows-5 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-3 px-6 md:px-12 lg:px-20 py-2">
+          <div className="grid grid-rows-5 gap-2 lg:gap-3 xl:gap-4 py-4">
             <div className="uppercase">Agentur und Leistungen</div>
             <Link href="/">Agentur</Link>
             <Link href="/de/leistungen">Leistungen</Link>
             <Link href="/">Referenzen</Link>
-            <div>
+            <div className="mt-1">
               <Link href="/" className="uppercase border border-white p-2">
                 <img src="/WhiteLine.png" className="inline" />
                 <span>&nbsp;Newsletter abonnieren</span>
               </Link>
             </div>
           </div>
-          <div className="grid grid-rows-5 gap-4">
+          <div className="grid grid-rows-5 gap-2 lg:gap-3 xl:gap-4 py-4">
             <div className="uppercase">Arbeiten bei Apevertising</div>
             <Link href="/de/heartworking">Heartworking</Link>
             <Link href="/de/jobs">Jobs</Link>
             <Link href="/de/beanape">#BEANAPE</Link>
           </div>
-          <div className="grid grid-rows-5 gap-4">
+          <div className="grid grid-rows-5 gap-2 lg:gap-3 xl:gap-4 py-4">
             <div className="uppercase">Kontakt</div>
             <div>APEvertising</div>
             <div>Lichtenhaidstr. 11a | 96052 Bamberg</div>
@@ -33,9 +33,10 @@ export function Footer() {
         </div>
       </div>
       <div className="bg-white text-black">
-        <div className="flex items-center justify-between container mx-auto px-24 py-4">
+        <div className="flex items-center justify-between container mx-auto px-6 md:px-12 lg:px-20 py-4">
           <div className="corporate-font1">
-            Impressum&nbsp;&nbsp;|&nbsp;&nbsp;Datenschutzerklärung&nbsp;&nbsp;|&nbsp;&nbsp;AGB&nbsp;&nbsp;|&nbsp;&nbsp;Hinweis nach VSBG
+            Impressum&nbsp;&nbsp;|&nbsp;&nbsp;Datenschutzerklärung&nbsp;&nbsp;|&nbsp;&nbsp;AGB&nbsp;&nbsp;|&nbsp;&nbsp;Hinweis
+            nach VSBG
           </div>
           <div className="flex">
             <div className="corporate-font1">Follow us</div>
